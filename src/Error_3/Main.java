148c547426c4bd7e83bf8038d93a12d85f79a8d4
TEACHER\PC_teacher <imakitakk23@gmail.com>
2023-06-06 14:56:22 +0900

    条件を追記

--- src/Error_3/Main.java
+++ src/Error_3/Main.java
@@ -2,7 +2,7 @@ package Error_3;
 
 public class Main {
     public static void main(String[] args) {
-        String message = null;
+        String message = null;//このコードは編集禁止
         int length = message.length();
         System.out.println("Message length: " + length);
     }
--- src/Error_5/Main.java
+++ src/Error_5/Main.java
@@ -2,6 +2,8 @@ package Error_5;
 
 public class Main {
     public static void main(String[] args) {
+    	//Class Bと表示されるように設定
+    	//※B objB = new B();は禁止
         A objA = new A();
         B objB = (B) objA;
         objB.print();
