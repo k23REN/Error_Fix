e77aa5ebd82215856da84cae59e4cef47cfad9f7
TEACHER\PC_teacher <imakitakk23@gmail.com>
2023-06-28 10:10:10 +0900

    0除算に変更

--- src/Error_6/Main.java
+++ src/Error_6/Main.java
@@ -2,7 +2,7 @@ package Error_6;
 
 public class Main {
     public static void main(String[] args) {
-        int result = calculateDivision(10, 2);
+        int result = calculateDivision(10, 0);
         System.out.println("Result: " + result);
     }
 
