213c53c4c39824bfeb02bd36d43c9bbd5717aeb9
TEACHER\PC_teacher <imakitakk23@gmail.com>
2023-06-28 12:05:58 +0900

    Error_2修正

--- src/Error_2/Main.java
+++ src/Error_2/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name:");
-        String name = scanner.nextInt();
+        String name = scanner.nextLine();
         System.out.println("Hello, " + name + "!");
         scanner.close();
     }
